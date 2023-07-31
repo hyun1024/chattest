b2c5af2f82d1845b9d22680ecb84ba4e3ef41719
hyun1024 <haema3043@gmail.com>
2023-07-31 17:52:23 +0900

    [#1] feat: 기능구현

--- src/main/java/com/sparta/springsecondwork/dto/BoardResponseDto.java
+++ src/main/java/com/sparta/springsecondwork/dto/BoardResponseDto.java
@@ -12,12 +12,12 @@ public class BoardResponseDto {
     private final String contents;
     private final LocalDateTime createdAt;
     private final LocalDateTime modifiedAt;
-    public BoardResponseDto(Board board) {
-        this.postId = board.getPostId();
-        this.subject=board.getSubject();
-        this.username=board.getUsername();
-        this.contents=board.getContents();
-        this.createdAt=board.getCreatedAt();
-        this.modifiedAt=board.getModifiedAt();
+    public BoardResponseDto(Long id, String subject, String username, String contents, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+        this.postId = id;
+        this.subject=subject;
+        this.username=username;
+        this.contents=contents;
+        this.createdAt=createdAt;
+        this.modifiedAt=modifiedAt;
     }
 }
